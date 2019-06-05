35c72632bc3fae9382d4a4c041a6ed0f4af45dfc
arun <arunreddyinfo@gmail.com>
2019-06-05 10:54:13 +0530

    terrafrom chaned

--- provider.tf
+++ provider.tf
@@ -2,14 +2,36 @@ provider "aws" {
   version = "~> 2.0"
   region  = "ap-south-1"
 }
+terraform{
+ backend "s3"{
+ bucket = "arunreddy123-tf-may"
+ key = "dev/javahomeapp/terraform.tfstate"
+ region = "ap-south-1"
+ }
+}
  //create VPC
- resource "aws_vpc" "myvpc" {
-  cidr_block       = "10.0.0.0/16"
+ resource "aws_vpc" "main" {
+  cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = "terraform-vpc"
     Batch = "930am"
-    Location = "Bangalore"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+#declareing a variable
+ variable "vpc_cidr"{
+ description = "choose cidr for vpc"
+  default = 10.0.0.0/16
+
+ }
